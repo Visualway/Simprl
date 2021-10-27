dc24001fe22328c0dba0d0d1317e86b68210dd89
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-10-27 13:26:49 +0530

    Added the Procfile and written test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import URL
 
 # Create your tests here.
+class URLTestCase(TestCase):
+    def setUp(self):
+        URL.objects.create(long_url="https://python.org")
+
+    def test_url_id(self):
+        python_site = URL.objects.get(long_url="https://python.org")
+        self.assertEqual(str(python_site), python_site.hash)
