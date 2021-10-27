209924ca675dd09764a5d3c43081b7d26c616d9e
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-10-27 18:18:50 +0530

    Update settings.py

--- Simprl/settings.py
+++ Simprl/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-41m@t&wqrqozc&%vek$7&=q*)6y++@2+3!orrie_ypnfyauska'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ['*']
 
