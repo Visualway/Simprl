aa62e21f38ac88f5da6dca3af54aeb904d6627d8
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-10-27 13:30:39 +0530

    Fixed the test

--- core/tests.py
+++ core/tests.py
@@ -4,8 +4,9 @@ from .models import URL
 # Create your tests here.
 class URLTestCase(TestCase):
     def setUp(self):
-        URL.objects.create(long_url="https://python.org")
-
+        url = URL(long_url="https://python.org")
+        url.save()
+        
     def test_url_id(self):
         python_site = URL.objects.get(long_url="https://python.org")
         self.assertEqual(str(python_site), python_site.hash)
