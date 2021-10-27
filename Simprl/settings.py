9b81ee7de0740ec1b816b7719cd39b6d40b0e0be
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-10-27 13:49:56 +0530

    Comfugred static

--- Simprl/settings.py
+++ Simprl/settings.py
@@ -116,18 +116,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# https://docs.djangoproject.com/en/3.STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIR = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if not DEBUG:
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)    
-STATICFILES_DIR = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
