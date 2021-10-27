dcf537a3487eb64c2557a54466847d8dd8611168
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-10-27 14:38:19 +0530

    Update settings.py

--- Simprl/settings.py
+++ Simprl/settings.py
@@ -122,9 +122,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if not DEBUG:
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)    
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)    
 STATICFILES_DIR = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
